a93c21f06a3e8b2737b4cea0479f11a19d1fc7f0
Philipp Middendorf <middendorf@plapadoo.de>
2018-09-10 17:42:54 +0200

    config_file: add “tag” element

--- ledgerclock/config_file.py
+++ ledgerclock/config_file.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Optional, List, Optional, Any, Dict
+from typing import NamedTuple, List, Optional, Any, Dict
 from pathlib import Path
 import json
 from xdg.BaseDirectory import xdg_config_home
@@ -6,12 +6,16 @@ from xdg.BaseDirectory import xdg_config_home
 
 class ConfigFile(NamedTuple):
     ledger_files: List[Path]
+    tag: str
 
 
-def read_config_file() -> Optional[ConfigFile]:
+def read_config_file() -> ConfigFile:
     config_path = Path(xdg_config_home) / "ledgerclock" / "config.json"
     if not config_path.exists():
-        return None
+        raise Exception('config file “' + str(config_path) + "” not found")
     with config_path.open() as f:
         json_obj: Dict[str, Any] = json.load(f)
-        return ConfigFile(ledger_files=json_obj['ledger_files'])
+        return ConfigFile(
+            ledger_files=json_obj['ledger_files'],
+            tag=json_obj['tag'],
+        )
