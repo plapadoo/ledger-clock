63cf71324194fe9c2729cbf0e72eefb31572ff14
Kim Rinnewitz <krinnewitz@plapadoo.de>
2018-09-10 16:28:10 +0200

    ledger: get_accounts, fuzzy_search, add_entry

--- ledgerclock/ledger.py
+++ ledgerclock/ledger.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from datetime import date, timedelta
 from typing import List, NamedTuple
+from fuzzyfinder import fuzzyfinder
 
 
 class Account(NamedTuple):
@@ -18,15 +19,26 @@ class LedgerEntry(NamedTuple):
 
 
 def get_accounts(filename: Path) -> List[Account]:
-    pass
+    with filename.open() as f:
+        account_lines = (line for line in f
+                         if line.upper().startswith('ACCOUNT '))
+        return [Account(line[8:]) for line in account_lines]
 
 
 def fuzzy_search(accounts: List[Account], needle: str) -> List[Account]:
-    pass
+    return (Account(result) for result in list(
+        fuzzyfinder(needle, [acc.content for acc in accounts])))
 
 
 def add_entry(filename: Path, entry: LedgerEntry) -> None:
-    pass
+    with filename.open('a') as f:
+        f.write('\n')
+        f.write('{} {}  ; {}\n'.format(entry.date.isoformat(), entry.payee,
+                                       entry.comment))
+        f.write('\t{}  {:2f}h  ; :{}:\n'.format(
+            entry.account,
+            entry.hours.total_seconds() / 3600, entry.user))
+        f.write('\t{}\n'.format(entry.account))  #TODO
 
 
 def add_account(filename: Path, name: str) -> None:
